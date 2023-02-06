562e48b2def80511b4aecbe56ec4ca3b7eed6d23
marcusH3 <marcus.hurtigh@outlook.com>
2023-02-06 08:18:05 +0100

    Password added to databaseconnection

--- src/DataBaseConnections/DataBaseConnection.java
+++ src/DataBaseConnections/DataBaseConnection.java
@@ -2,11 +2,13 @@ package DataBaseConnections;
 
 import Database.*;
 
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -16,7 +18,6 @@ private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customers (custom
         ",customerAddress,customerEmail,customerPassword,customerPhoneNumber,CityID) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
     private static final String INSERT_CITY_SQL = "INSERT INTO city (cityName) VALUES (?)";
-    private static final String sql_string = "call addTocart(?,?,?)";
 
     private static Properties properties;
 
@@ -47,6 +48,9 @@ private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customers (custom
         }
         return false;
     }  // TODO: 2023-02-01  If login not valid, return false and present error message or direct to create new user.
+
+
+
 
     public static List<GenderCategory> getGender(String username, String password){
 
@@ -371,7 +375,7 @@ private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customers (custom
         try(Connection con = DriverManager.getConnection(
                 properties.getProperty("urlString"), username,password)){
             Statement statement = con.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT customerID, customerFirstName,customerLastName,customerAddress, customerEmail, customerPhoneNumber, cityID from customers");
+            ResultSet resultSet = statement.executeQuery("SELECT customerID, customerFirstName,customerLastName,customerAddress, customerEmail, customerPassword,customerPhoneNumber, cityID from myseconddatabase.customers");
 
             while(resultSet.next()){
                 int customerID = resultSet.getInt("customerID");
@@ -379,10 +383,11 @@ private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customers (custom
                 String lastName = resultSet.getString("customerLastName");
                 String customerAddress = resultSet.getString("customerAddress");
                 String customerEmail = resultSet.getString("customerEmail");
+                String customerPassword = resultSet.getString("customerPassword");
                 String customerPhoneNumber = resultSet.getString("customerPhoneNumber");
                 int cityID = resultSet.getInt("cityID");
 
-                Customer customer = new Customer(customerID, firstName,lastName, customerPhoneNumber, customerEmail,null, customerAddress,cityID);
+                Customer customer = new Customer(customerID, firstName,lastName,customerPhoneNumber, customerEmail,customerPassword, customerAddress,cityID);
                 customers.add(customer);
 
             }
@@ -393,6 +398,71 @@ private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customers (custom
         return customers;
     }
 
+public static void connectAndQueryDB(String username, String password){
+
+        try {
+            properties = new Properties();
+            InputStream input = new FileInputStream("Res/config.properties");
+            properties.load(input);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    try(Connection con = DriverManager.getConnection(
+            properties.getProperty("urlString"), username,password)){
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT customerFirstName,customerLastName from customers");
+
+            while(resultSet.next()){
+                String firstName = resultSet.getString("customerFirstName");
+                String lastName = resultSet.getString("customerLastName");
+
+                System.out.println(lastName+", "+firstName+"\n");
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+}
+    /*    public class DatabaseManager {
+        private final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        private final String DB_URL = "jdbc:mysql://hostname:port/database";
+        private final String USER = "username";
+        private final String PASS = "password";
+
+        public boolean authenticate(String username, String password) {
+            boolean isAuthenticated = false;
+
+            try {
+                Class.forName(JDBC_DRIVER);
+
+                // Open a connection
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+                // Execute SQL query
+                String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, username);
+                statement.setString(2, password);
+                ResultSet result = statement.executeQuery();
+
+                if (result.next()) {
+                    isAuthenticated = true;
+                }
+                result.close();
+                statement.close();
+                conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return isAuthenticated;
+        }
+    }*/
     public static List<Order> getOrders(String username, String password){
 
         List<Order> orderList = new ArrayList<>();
@@ -622,29 +692,61 @@ private static final String INSERT_CUSTOMER_SQL = "INSERT INTO customers (custom
         }
     }
 
-    public static void callProcedure(String username, String password, int orderID, int customerID, int productID){
+       /* Data Access Object (DAO): A DAO is a class that abstracts the details of how the data is stored in the database,
+        and provides a set of methods that the rest of the program can use to perform CRUD (Create, Read, Update, Delete)
+        operations on the data.
 
-        try {
-            properties = new Properties();
-            InputStream input = new FileInputStream("res/config.properties");
-            properties.load(input);
-        }
-        catch (IOException e){
+        Database Connection: A class that handles connecting to the database and managing the connection. This class typically
+        uses a JDBC (Java Database Connectivity) driver to connect to the database.
+        Data Transfer Object (DTO): A DTO is a simple class that holds the data retrieved from the database and passed between
+        the program and the DAO.
+
+        Business Logic: The main class that handles the business logic of the program, it uses the DAO class to interact with the
+        database and DTO classes to hold the data.
+
+        Main: The main class that runs the program, it creates an instance of the business logic class and calls the methods to
+        perform the desired operations.*/
+
+
+    public static void addToCart(Integer currentCustomerOrder, Customer whoIsLoggedIn, int id) {
+        try (Connection con = createConnection();
+             CallableStatement cstmt = con.prepareCall("{CALL addToCart(?,?,?)}")) {
+
+            cstmt.setInt(1, currentCustomerOrder);
+            cstmt.setInt(2, whoIsLoggedIn.getCustomerID());
+            cstmt.setInt(3, id);
+            cstmt.execute();
+
+
+
+           /*.showMessage("Added to cart", "New item added to  cart", null, Alert.AlertType.INFORMATION);
+        } catch (SQLException e) {
             e.printStackTrace();
-        };
+        } catch (NullPointerException e){
+            if (!.isLoggedIn){
+                .showMessage("Not logged in!", "You need to be logged in\nto make and order", null, Alert.AlertType.ERROR);
+            }else {
+                .showMessage("No Order", "You need to create a order first,\nbefore you can add products", null, Alert.AlertType.ERROR);
+            }
 
-        try(Connection con = DriverManager.getConnection(
-                properties.getProperty("urlString"), username,password)){
+        }*/
 
-
-            CallableStatement cs = con.prepareCall(sql_string);
-            cs.setNull(2, Types.INTEGER);
-            cs.setInt(1, customerID);
-            cs.setInt(3, productID);
-            cs.execute();
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-    }
+        public static void removeFromCart (Integer currentCustomerOrder, Customer whoIsLoggedIn,int id){
 
+            try (Connection con = createConnection();
+                 CallableStatement cstmt = con.prepareCall("{CALL remove_form_cart(?,?,?)}")) {
+
+                cstmt.setInt(1, currentCustomerOrder);
+                cstmt.setInt(2, whoIsLoggedIn.getCustomerID());
+                cstmt.setInt(3, id);
+                cstmt.execute();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+}
